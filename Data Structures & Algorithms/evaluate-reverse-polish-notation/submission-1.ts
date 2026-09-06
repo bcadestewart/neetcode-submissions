af2d2760c6b4d4d304ca6de383fed69f70e1afe8
Brandon Stewart <88789552+bcadestewart@users.noreply.github.com>
2026-09-06 16:59:29 -0500

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.ts
@@ -0,0 +1,36 @@
+class Solution {
+  /**
+   * @param {string[]} tokens
+   * @return {number}
+   */
+  evalRPN(tokens: string[]): number {
+    const stack: number[] = [];
+
+    for (const token of tokens) {
+      if (token === "+" || token === "-" || token === "*" || token === "/") {
+        const b = stack.pop()!;
+        const a = stack.pop()!;
+
+        if (token === "+") {
+          stack.push(a + b);
+        }
+
+        if (token === "-") {
+          stack.push(a - b);
+        }
+
+        if (token === "*") {
+          stack.push(a * b);
+        }
+
+        if (token === "/") {
+          stack.push(Math.trunc(a / b));
+        }
+      } else {
+        stack.push(Number(token));
+      }
+    }
+
+    return stack[0];
+  }
+}
